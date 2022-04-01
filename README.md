9cefc14f89c0e738fb3b1f88c7263db520dc65d6
ap23hp <aprakhanna5@gmail.com>
2022-04-01 13:57:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-rv3edi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-rv3edi)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+import ControlledForm from './ControlledForms';
+
+export default function App() {
+  return (
+    <div>
+      <ControlledForm />
+    </div>
+  );
+}
